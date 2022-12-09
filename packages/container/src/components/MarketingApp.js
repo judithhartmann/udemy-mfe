4585f9d45ff92118d8aa1ed2ae626f3b3ed7e1a4
Judith Hartmann <mail@judith-hartmann.net>
2022-12-09 15:05:05 +0100

    sync history between container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,24 @@
 import React, { useRef, useEffect } from "react";
 import { mount as mountMarketing } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mountMarketing(ref.current);
-  }, []);
+    const { onParentNavigate } = mountMarketing(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, [history]);
 
   return <div ref={ref} />;
 };
